22981501ed0d84b5505627809d05ab0eba058a77
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-04-29 13:36:42 +0200

    Updates

---
+++ precache-manifest.586a4a00d5f38b9b07954a7fbcb73f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e4b011e96fa9762f0afd4234c830dc",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "82b61dd18a778553ae81",
+    "url": "/react-router/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "28034ad3ed4c7cbe5456",
+    "url": "/react-router/static/css/main.3b116a29.chunk.css"
+  },
+  {
+    "revision": "82b61dd18a778553ae81",
+    "url": "/react-router/static/js/2.91e9f710.chunk.js"
+  },
+  {
+    "revision": "28034ad3ed4c7cbe5456",
+    "url": "/react-router/static/js/main.59dc9706.chunk.js"
+  },
+  {
+    "revision": "c6984152ca93223c2cb4",
+    "url": "/react-router/static/js/runtime~main.fdc07815.js"
+  }
+]);
